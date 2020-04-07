ad62d85931341fad67c7aefdde553eed6c55e1e1
Jakob Bossek <info@jakobbossek.de>
2020-04-07 13:03:26 +0930

    update getTourByIncreasingWeight (add greedily by distance if several nodes have the same weight)

--- R/getTourByIncreasingWeight.R
+++ R/getTourByIncreasingWeight.R
@@ -16,15 +16,65 @@
 getTourByIncreasingWeight = function(prob, packing) {
   checkmate::assertClass(prob, "ttp_instance")
   checkmate::assertIntegerish(packing, len = prob$m, lower = 0, upper = 1, any.missing = FALSE, all.missing = FALSE)
+  n = prob$n
+  D = prob$distance.matrix
 
   # get weights per node
   wbn = getWeightByNodeC(prob, packing, prob$items$weight, prob$items$nodenr)
 
   # now form tour: 1st node is 1 and the following are the remaining nodes
   # in their order of weight. Note that +1 here is necessary!
-  tour = c(1, order(wbn[-1L], decreasing = FALSE) + 1L)
+  ord = c(1L, order(wbn[-1L], decreasing = FALSE) + 1L)
+  wbnord = wbn[ord]
+  # now choose greedily closest node if there are several with the same weight
+  tour = c(1, rep(NA, n - 1L))
+  i = 2L
+  print(wbn)
+  print(ord)
+  print(wbn[ord])
+  while (i <= n) {
+    l = i
+    u = i
+    cw = wbnord[i]
+    # find block with all equal weights
+    while ((u + 1) <= n) {
+      if (wbnord[u+1] == cw) {
+        u = u + 1L
+      } else {
+        break
+      }
+    }
+    #catf("Block length: %i, weight: %i\n", u-l+1, cw)
+    # current block has length 1
+    if (l == u) {
+      # no choice: there is just one node with the current weight
+      tour[i] = ord[i]
+      i = i + 1L
+    } else {
+      # there are > 1 nodes with the same weight -> Add these in
+      # their order of distance
+      p = l
+      cands = ord[l:u]
+      while (p <= u) {
+        source = tour[p-1L]
+        dists = D[source, cands]
+        idx.min = which.min(dists)
+        tour[p] = cands[idx.min]
+
+        cands = setdiff(cands, cands[idx.min])
+        p = p + 1L
+      }
+      i = u + 1L
+    }
+  }
+
+  #tour = c(1, order(wbn[-1L], decreasing = FALSE) + 1L)
+  print(tour)
+  kkk <<- tour
   stopifnot(re::is.permutation(tour, s = 1:prob$n))
   #print(tour)
   #print(wbn[tour])
+
+  #tour = getTourByIncreasingWeightC(prob, packing)
   return(tour)
 }
